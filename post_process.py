a734362c93e770686f49ee29b572cf15122889f3
halo-framework <halo-framework@gmail.com>
2020-12-11 16:27:54 +0200

    first

--- post_process.py
+++ post_process.py
@@ -12,12 +12,22 @@ else:
     file2.write("missing:"+file_path+"\n")
 if file_path.find("controller_service") > 0:
     dir = os.path.dirname(file_path)
+    file2.write("dir:"+dir+"\n")
     file_name = os.path.basename(file_path)
+    file2.write("file_name:"+file_name+"\n")
     parent_dir = os.path.dirname(dir)
+    file2.write("parent_dir:"+parent_dir+"\n")
     target_dir = os.path.join(parent_dir,"mixin")
-    new_name=file_name.replace("_controller_service.py","_service.py")
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    file2.write("target:"+target_dir+"\n")
+    new_name=file_name#.replace("_controller_service.py","_service.py")
     new_path = os.path.join(target_dir,new_name)
-    os.rename(file_path, new_path)
-    #shutil.copyfile(file_name, file_name+".xyz")
+    file2.write("new_path:"+new_path+"\n")
+    try:
+        #os.rename(file_path, new_path)
+        shutil.move(file_path, new_path)
+    except Exception as e:
+        file2.write("error:"+str(e)+"\n")
     file2.write("fixed:"+new_path+"\n")
 file2.close()
