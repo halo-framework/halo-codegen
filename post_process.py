5a20cbd418a3446685edd21cb6c0f63dff8896e7
halo-framework <halo-framework@gmail.com>
2020-12-12 18:12:37 +0200

    first

--- post_process.py
+++ post_process.py
@@ -5,52 +5,36 @@ import os
 import shutil
 
 file_path=sys.argv[1]
-file2 = open(r"c:\dev\MyFile2.txt", "a")
-#if os.path.exists(file_path):
-#    file2.write(file_path+"\n")
-#else:
-#    file2.write("missing:"+file_path+"\n")
+dir = os.path.dirname(file_path)
+parent_dir = os.path.dirname(dir)
+file_name = os.path.basename(file_path)
+log_path = os.path.join(parent_dir,"log_file.txt")
+file1 = open(log_path, "a")
+file1.write(file_path+"\n")
 if file_path.find("controller_service") > 0:
-    dir = os.path.dirname(file_path)
-    #file2.write("dir:"+dir+"\n")
-    file_name = os.path.basename(file_path)
-    #file2.write("file_name:"+file_name+"\n")
-    parent_dir = os.path.dirname(dir)
-    #file2.write("parent_dir:"+parent_dir+"\n")
+    file1.write("service:"+file_path+"\n")
     target_dir = os.path.join(parent_dir,"services")
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
-    #file2.write("target:"+target_dir+"\n")
     new_name = file_name.replace("_controller_service.py","_service.py")
     new_path = os.path.join(target_dir,new_name)
-    #file2.write("new_path:"+new_path+"\n")
     try:
         #os.rename(file_path, new_path)
         shutil.move(file_path, new_path)
     except Exception as e:
-        #file2.write("error:"+str(e)+"\n")
+        file1.write("error:"+str(e)+"\n")
         pass
-    #file2.write("fixed:"+new_path+"\n")
-#file2.close()
-file2.write(file_path+"\n")
 if file_path.find("_controller_json") > 0:
-    dir = os.path.dirname(file_path)
-    file2.write("dir:"+dir+"\n")
-    file_name = os.path.basename(file_path)
-    #file2.write("file_name:"+file_name+"\n")
-    parent_dir = os.path.dirname(dir)
-    #file2.write("parent_dir:"+parent_dir+"\n")
+    file1.write("jaon:"+file_path+"\n")
     target_dir = os.path.join(parent_dir,"config")
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
-    #file2.write("target:"+target_dir+"\n")
     new_name = file_name.replace("_controller_json.py",".json")
     new_path = os.path.join(target_dir,new_name)
-    #file2.write("new_path:"+new_path+"\n")
     try:
         #os.rename(file_path, new_path)
         shutil.move(file_path, new_path)
     except Exception as e:
-        #file2.write("error:"+str(e)+"\n")
+        file1.write("error:"+str(e)+"\n")
         pass
-    #file2.write("fixed:"+new_path+"\n")
+file1.close()
